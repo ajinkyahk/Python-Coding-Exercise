2c118f741b79430bc917d81b07098e66ce7e2f9c
Ajinkya Kulkarni <95011676+ajinkyahk@users.noreply.github.com>
2022-05-04 23:02:47 +0530

    Create Data-Overlap.py

---
+++ Exercise 4/Data-Overlap.py
@@ -0,0 +1,12 @@
+#Exercise - Data Overlap
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above
+
+#Write your 1 line code 👇 below:
+result=list(filter(lambda x:x%2==0, numbers))
+
+#Write your code 👆 above:
+
+print(result)
+
