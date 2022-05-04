0b6cc229a891e3aa04638702a6cc6ba9ca33e0d4
Ajinkya Kulkarni <95011676+ajinkyahk@users.noreply.github.com>
2022-05-04 23:05:45 +0530

    Create squaring-numbers.py

---
+++ Exercise 5/squaring-numbers.py
@@ -0,0 +1,13 @@
+#Exercise - Squaring Numbers
+
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+#Write your 1 line code 👇 below:
+squared_numbers=list(map(lambda x: x**2 , numbers))
+
+#Write your code 👆 above:
+
+print(squared_numbers)
+
